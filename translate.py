a33a86add66a76b270d2de0466ca8d98849cef50
allison <allison@all-Book.(none)>
2013-02-01 11:44:58 -0200

    Implementada a entrada e o controle dos dados que serão passados pelo usuário. Utilização da estrutura de dados Dicionário para melhorar a interação com o programa. Falta consertar o bug dos caracteres especiais retornados pelo site.

--- translate.py
+++ translate.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import http.client
+import sys
 
-#Função para identar e facilitar leituda da string
+#Função apenas para identar e facilitar leituda da string
 def ident_str(id_str):
 	counter = 0
 
@@ -50,8 +51,20 @@ def result_str(my_str, item):
 #my_str = '''[[["teste","test","",""]],[["substantivo",["teste","ensaio","prova","exame","análise","experiência","verificação","padrão","bitola","craveira","pedra-de-toque"],[["teste",["test","assay","audition","try","try-out"],,0.39777252],["ensaio",["test","assay","testing","trial","essay","rehearsal"],,0.093014486],["prova",["proof","evidence","test","trial","testing","competition"],,0.023153137],["exame",["examination","exam","test","review","survey","inspection"],,0.015423315],["análise",["analysis","review","parsing","test","assay","decomposition"],,0.0034414066],["experiência",["experience","experiment","background","trial","test","experimentation"],,0.00074425637],["verificação",["verification","examination","inspection","test","proofing","proof"],,0.00057962746],["padrão",["standard","pattern","model","template","type","test"],,6.0145136e-05],["bitola",["gauge","gage","diameter","test","scantling"],,2.2959243e-06],["craveira",["standard","test"],,8.7143462e-07],["pedra-de-toque",["touchstone","shibboleth","test"],,4.247033e-07]]],["verbo",["ensaiar","verificar","experimentar","provar","examinar","pôr à prova","submeter a teste","analisar com reagente"],[["ensaiar",["test","rehearse","try","assay","experiment","essay"],,0.00062672672],["verificar",["check","verify","see","ascertain","find","test"],,0.00013136906],["experimentar",["experience","try","experiment","try out","sample","test"],,8.4818232e-05],["provar",["prove","taste","sample","show","evidence","test"],,7.1424118e-05],["examinar",["examine","look","scan","check","explore","test"],,4.5399931e-05],["pôr à prova",["test","prove","proof","tax","tempt","match"],,6.8543641e-06],["submeter a teste",["test"],,2.9944499e-06],["analisar com reagente",["test"],,8.990969e-07]]]],"en",,[["teste",[5],1,0,1000,0,1,0]],[["test",4,,,""],["test",5,[["teste",1000,1,0],["ensaio",0,1,0],["teste de",0,1,0],["de teste",0,1,0],["de ensaio",0,1,0]],[[0,4]],"test"]],,,[["en"]],34]'''
 #ident_str (my_str)
 
+
+if len(sys.argv) < 2:
+	print ("Uso: translate [palavra em inglês]")
+	sys.exit()
+
+if sys.argv[1].find(" ") != -1 or len(sys.argv) > 2:
+	print("Escreva apenas uma palavra para tradução...")
+	sys.exit()
+
+word = sys.argv[1]
+url = "http://translate.google.com/translate_a/t?client=t&text=" + word + "&sl=en&tl=pt"
+
 conn = http.client.HTTPConnection("www.translate.google.com")
-conn.request("GET", "http://translate.google.com/translate_a/t?client=t&text=file&sl=en&tl=pt")
+conn.request("GET", url)
 
 r1 = conn.getresponse()
 my_str = r1.read()
@@ -62,12 +75,11 @@ my_str = str(my_str)
 my_str = repr(my_str)
 
 #Tupla representando a classificação das palavras
-#classes = ("substantivo", "pronome", "adjetivo", "artigo")
-classes = ("noun", "word", "adjective", "article")
+classes = {"noun":"substantivo", "pronoun":"pronome", "adjective":"adjetivo", "article":"artigo"}
 
 for my_class in classes:
 	result = result_str(my_str, my_class)
 	if result != None:
-		print (my_class, ":")
+		print (classes[my_class], ":")
 		print (result)
 		print ("")
